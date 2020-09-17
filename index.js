1ab3069e8754d787ed37eca7b7826dc59a849a10
jonathan12352 <37117994+jonathan12352@users.noreply.github.com>
2020-09-18 00:03:04 +0800

    Init commit

---
+++ index.js
@@ -0,0 +1,54 @@
+'use strict';
+
+const line = require('@line/bot-sdk');
+const express = require('express');
+const AWS = require('aws-sdk');
+
+// create LINE SDK config from env variables
+const config = {
+  channelAccessToken: process.env.CHANNEL_ACCESS_TOKEN,
+  channelSecret: process.env.CHANNEL_SECRET,
+};
+
+//AWS Config
+AWS.config.update({region: 'ap-southeast-1'});
+
+// Create S3 service object
+s3 = new AWS.S3({apiVersion: '2006-03-01'});
+
+// create LINE SDK client
+const client = new line.Client(config);
+
+const app = express();
+
+// register a webhook handler with middleware
+// about the middleware, please refer to doc
+app.post('/callback', line.middleware(config), (req, res) => {
+  Promise
+    .all(req.body.events.map(handleEvent))
+    .then((result) => res.json(result))
+    .catch((err) => {
+      console.error(err);
+      res.status(500).end();
+    });
+});
+
+// event handler
+function handleEvent(event) {
+  if (event.type !== 'message' || event.message.type !== 'text') {
+    // ignore non-text-message event
+    return Promise.resolve(null);
+  }
+
+  // create a echoing text message
+  const echo = { type: 'text', text: 'Echo from Server: ' + event.message.text};
+
+  // use reply API
+  return client.replyMessage(event.replyToken, echo);
+}
+
+// listen on port
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log(`listening on ${port}`);
+});
